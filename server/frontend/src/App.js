95eb2add00c1ce1fcefc8f557498743af42c688a
KenichiroArai <sammyleejunior@gmail.com>
2024-11-28 07:04:39 -0500

    M4_ディーラーページ用の REACT コンポーネントを追加して設定する

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,6 +1,6 @@
 import LoginPanel from "./components/Login/Login"
 import RegisterPanel from "./components/Register/Register"
-import Dealers from './components/Dealers/Dealers';
+import DealersPanel from './components/Dealers/Dealers';
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
     <Routes>
     <Route path="/login" element={<LoginPanel />} />
     <Route path="/register" element={<RegisterPanel />} />
-    <Route path="/dealers" element={<Dealers/>} />
+    <Route path="/dealers" element={<DealersPanel />} />
     </Routes>
   );
 }
