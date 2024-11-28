41880f0d8e92e8f6beb14c43a3322ebeeb6baa2b
KenichiroArai <sammyleejunior@gmail.com>
2024-11-28 09:54:48 -0500

    M4_ディーラーの詳細またはレビューページを作成する

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -2,6 +2,7 @@ import LoginPanel from "./components/Login/Login"
 import RegisterPanel from "./components/Register/Register"
 import DealersPanel from './components/Dealers/Dealers';
 import DealerPanel from './components/Dealers/Dealer';
+import PostReviewPanel from "./components/Dealers/PostReview"
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path="/register" element={<RegisterPanel />} />
         <Route path="/dealers" element={<DealersPanel />} />
         <Route path="/dealer/:id" element={<DealerPanel />} />
+        <Route path="/postreview/:id" element={<PostReviewPanel />} />
     </Routes>
   );
 }
