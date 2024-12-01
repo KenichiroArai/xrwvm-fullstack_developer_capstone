fb4b8ded68395f71867356cc820e1a832b13963c
KenichiroArai <sammyleejunior@gmail.com>
2024-12-01 00:04:47 -0500

    M7_パート 3: 車両在庫サービスのフロントエンド開発。

--- server/frontend/src/components/Dealers/Dealer.jsx
+++ server/frontend/src/components/Dealers/Dealer.jsx
@@ -10,81 +10,59 @@ import Header from '../Header/Header';
 
 const Dealer = () => {
 
+    const [dealer, setDealer] = useState({});
+    const [reviews, setReviews] = useState([]);
+    const [unreviewed, setUnreviewed] = useState(false);
+    const [postReview, setPostReview] = useState(<></>)
 
-  const [dealer, setDealer] = useState({});
-  const [reviews, setReviews] = useState([]);
-  const [unreviewed, setUnreviewed] = useState(false);
-  const [postReview, setPostReview] = useState(<></>)
-
-  let curr_url = window.location.href;
-  let root_url = curr_url.substring(0, curr_url.indexOf("dealer"));
-  let params = useParams();
-  let id =params.id;
-  let dealer_url = root_url + `djangoapp/dealer/${id}`;
-  let reviews_url = root_url + `djangoapp/reviews/dealer/${id}`;
-  let post_review = root_url + `postreview/${id}`;
+    let curr_url = window.location.href;
+    let root_url = curr_url.substring(0, curr_url.indexOf("dealer"));
+    let params = useParams();
+    let id =params.id;
+    let dealer_url = root_url + `djangoapp/dealer/${id}`;
+    let reviews_url = root_url + `djangoapp/reviews/dealer/${id}`;
+    let post_review = root_url + `postreview/${id}`;
   
-  const get_dealer = async ()=>{
+    const get_dealer = async ()=>{
+        const res = await fetch(dealer_url, {
+            method: "GET"
+        });
+        const retobj = await res.json();
+        
+        if (retobj.status === 200) {
+            let dealerobjs = [retobj.dealer];
 
-    // TODO 2024/11/28 デバッグ出力
-    console.log('Dealer.jsx:', 'get_dealer', dealer_url);
-
-    const res = await fetch(dealer_url, {
-      method: "GET"
-    });
-    const retobj = await res.json();
-
-    // TODO 2024/11/28 デバッグ出力
-    console.log('Dealer.jsx:', 'get_dealer', 'retobj', retobj);
-    
-    if(retobj.status === 200) {
-        //  2024/11/28 変更
-        //   let dealerobjs = Array.from(retobj.dealer);
-        let dealerobjs = [retobj.dealer];
-
-      // TODO 2024/11/28 デバッグ出力
-      console.log('Dealer.jsx:', 'get_dealer', 'retobj.dealer', retobj.dealer);
-      console.log('Dealer.jsx:', 'get_dealer', 'dealerobjs', dealerobjs);
-      console.log('Dealer.jsx:', 'get_dealer', 'dealerobjs[0]', dealerobjs[0]);
-
-      setDealer(dealerobjs[0])
+            setDealer(dealerobjs[0])
+        }
     }
-  }
 
-  const get_reviews = async ()=>{
-
-    // TODO 2024/11/28 デバッグ出力
-    console.log('Dealer.jsx:', 'get_reviews', reviews_url);
-
-    const res = await fetch(reviews_url, {
-      method: "GET"
-    });
-    const retobj = await res.json();
-    
-    if(retobj.status === 200) {
-      if(retobj.reviews.length > 0){
-        setReviews(retobj.reviews)
-      } else {
-        setUnreviewed(true);
-      }
+    const get_reviews = async ()=>{
+        const res = await fetch(reviews_url, {
+            method: "GET"
+        });
+        const retobj = await res.json();
+        
+        if (retobj.status === 200) {
+            if (retobj.reviews.length > 0){
+                setReviews(retobj.reviews)
+            } else {
+                setUnreviewed(true);
+            }
+        }
     }
-  }
 
-  const senti_icon = (sentiment)=>{
-    let icon = sentiment === "positive"?positive_icon:sentiment==="negative"?negative_icon:neutral_icon;
-    return icon;
-  }
-
-  useEffect(() => {
-    get_dealer();
-    get_reviews();
-    if(sessionStorage.getItem("username")) {
-      setPostReview(<a href={post_review}><img src={review_icon} style={{width:'10%',marginLeft:'10px',marginTop:'10px'}} alt='Post Review'/></a>)
-
-      
+    const senti_icon = (sentiment)=>{
+        let icon = sentiment === "positive"?positive_icon:sentiment==="negative"?negative_icon:neutral_icon;
+        return icon;
     }
-  },[]);  
 
+    useEffect(() => {
+        get_dealer();
+        get_reviews();
+        if (sessionStorage.getItem("username")) {
+            setPostReview(<a href={post_review}><img src={review_icon} style={{width:'10%',marginLeft:'10px',marginTop:'10px'}} alt='Post Review'/></a>)
+        }
+    }, []);
 
     return (
         <div style={{margin:"20px"}}>
@@ -93,18 +71,19 @@ const Dealer = () => {
                 <h1 style={{color:"grey"}}>{dealer.full_name}{postReview}</h1>
                 <h4 style={{color:"grey"}}>{dealer['city']},{dealer['address']}, Zip - {dealer['zip']}, {dealer['state']} </h4>
             </div>
+            <a href={`/searchcars/${id}`}>SearchCars</a>
             <div class="reviews_panel">
-            {reviews.length === 0 && unreviewed === false ? (
+                {reviews.length === 0 && unreviewed === false ? (
                 <text>Loading Reviews....</text>
-            ):  unreviewed === true? <div>No reviews yet! </div> :
-            reviews.map(review => (
-            <div className='review_panel'>
-                <img src={senti_icon(review.sentiment)} className="emotion_icon" alt='Sentiment'/>
-                <div className='review'>{review.review}</div>
-                <div className="reviewer">{review.name} {review.car_make} {review.car_model} {review.car_year}</div>
+                ):  unreviewed === true? <div>No reviews yet! </div> :
+                reviews.map(review => (
+                <div className='review_panel'>
+                    <img src={senti_icon(review.sentiment)} className="emotion_icon" alt='Sentiment'/>
+                    <div className='review'>{review.review}</div>
+                    <div className="reviewer">{review.name} {review.car_make} {review.car_model} {review.car_year}</div>
+                </div>
+                ))}
             </div>
-            ))}
-            </div>  
         </div>
     )
 }
